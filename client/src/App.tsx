8fa0ffa33af5fec1acb513200b5371ab27257d86
Ritik dangi <ritikdangi445566@gmail.com>
2025-12-24 12:56:56 +0530

    initial client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className=" p-5 text-red-500">
+      <h1 className="text-2xl font-bold">QwikConsult Client</h1>
+    </div>
+  );
+}
+
+export default App;
