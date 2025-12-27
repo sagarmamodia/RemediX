20a19168e8a4d7f5209a8a48678c54aa022b22c9
Sagar Mamodia <sagar74822@gmail.com>
2025-12-27 17:07:08 +0530

    commented the mail testing endpoint

--- server/app.ts
+++ server/app.ts
@@ -8,7 +8,6 @@ import consultationRoutes from "./src/routes/consultation.routes";
 import doctorRoutes from "./src/routes/doctor.routes";
 import patientRoutes from "./src/routes/patient.routes";
 import profileRoutes from "./src/routes/profile.routes";
-import { sendMail } from "./src/utils/mail";
 
 const app = express();
 
@@ -38,29 +37,6 @@ app.get("/api/health", async (req: Request, res: Response) => {
     .json({ success: true, data: { message: "health OK!" } });
 });
 
-// TEST THE MAIL SERVICE
-app.post("/api/testMail/:mailId", async (req: Request, res: Response) => {
-  try {
-    const mailId = req.params.mailId;
-    if (!mailId)
-      return res
-        .status(400)
-        .json({ success: false, data: { error: "mail id missing" } });
-    await sendMail(
-      mailId,
-      "This is a test mail to verify that mail service is working",
-      { html: "<h1>Mail service is working</h1>" }
-    );
-    return res
-      .status(200)
-      .json({ success: true, data: { error: "Email sent" } });
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ success: false, data: { error: "Email not sent" } });
-  }
-});
-
 // AUTH ROUTES
 app.use("/api/auth", authRoutes);
 
@@ -75,6 +51,29 @@ app.use("/api/patient", patientRoutes);
 
 // ROUTES RELATED TO CONSULTATION
 app.use("/api/consultation", consultationRoutes);
+
+// TEST THE MAIL SERVICE
+// app.post("/api/testMail/:mailId", async (req: Request, res: Response) => {
+//   try {
+//     const mailId = req.params.mailId;
+//     if (!mailId)
+//       return res
+//         .status(400)
+//         .json({ success: false, data: { error: "mail id missing" } });
+//     await sendMail(
+//       mailId,
+//       "This is a test mail to verify that mail service is working",
+//       { html: "<h1>Mail service is working</h1>" }
+//     );
+//     return res
+//       .status(200)
+//       .json({ success: true, data: { error: "Email sent" } });
+//   } catch (err) {
+//     return res
+//       .status(500)
+//       .json({ success: false, data: { error: "Email not sent" } });
+//   }
+// });
 
 // Global Error Handler
 app.use(globalErrorHandler);
